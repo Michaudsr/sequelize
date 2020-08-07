611e95a640d2adcb6f677f9be80666a1b3884dc0
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-08-07 15:25:23 -0400

    [sequelize]

--- app.js
+++ app.js
@@ -7,4 +7,67 @@ db.user.create({
     email: 'michaudsr@gmail.com'
 
 
+}).then(data =>{
+    console.log(data);
+})
+
+db.user.findOne({
+    where: { id: 1}
+
+}).then(user =>{
+    console.log(user.dataValues);
+
 });
+
+db.user.findOne({
+    where: { firstName: 'Steven'}
+
+}).then(user =>{
+    console.log(user.dataValues);
+})
+
+db.user.findOne({
+    where: { email: 'michaudsr@gmail.com'}
+}).then(user =>{
+    console.log(user.dataValues);
+})
+
+db.user.findOrCreate({
+    where: {
+        firstName: 'Tyler',
+        lastName: 'Landers'
+
+    },
+    defaults: { age: 27, email: 'tylerlanders@gmail.com'}
+}).then(([user, created])=>{
+    console.log(`This was created on ${created}`);
+    console.log(user.dataValues);
+})
+
+let queryOne = {
+    where: {
+        firstName: 'John',
+        lastName: 'Smight'
+
+    },
+    defaults: { age: 25, email: 'johnsmith@aol.com'}
+};
+
+db.user.findOrCreate(queryOne)
+.then(([user, created])=>{
+    console.log(created);
+    console.log(user.dataValues);
+});
+
+db.user.findAll()
+.then (users => {
+    console.log(users);
+
+    for (let i = 0; i < users.length; i ++) {
+        let eachUser = users[i].dataValues;
+        // console.log(eachUser);
+        let firstName = users[i].dataValues.firstName;
+        console.log(firstName);
+    }
+})
+  
